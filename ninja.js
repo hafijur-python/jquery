d2fb032b708aae371d72e117695f480545c7d556
hafijurrhaman <sun201428@yahoo.com>
2018-03-02 15:50:58 -0800

    jaqury file

---
+++ ninja.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+        
+        $('.pictur').click(function(){
+            
+           $(this).hide();
+            
+        });
+        
+        $('button').click(function(){
+            
+            $('.pictur').show('.pictur');
+             
+         });
+    });
+    
+
